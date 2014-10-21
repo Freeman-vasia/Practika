81f8db9ef9e47d6dbc0f0d4a50e8731e24202885
Misha <drohomiretskiy@gmail.com>
2014-10-22 00:22:37 +0300

    Update about.php

--- www/application/views/pages/about.php
+++ www/application/views/pages/about.php
@@ -1,3 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Laura John</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+<link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
+<link href="http://fonts.googleapis.com/css?family=PT+Sans:400italic" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=PT+Sans:700" rel="stylesheet" type="text/css">
+<script src="js/jquery-1.7.1.min.js"></script>
+<script src="js/tms-0.3.js"></script>
+<script src="js/tms_presets.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<!--[if lt IE 9]><script src="js/html5.js"></script><![endif]-->
+</head>
+<body id="page1">
+<!--==============================header=================================-->
+<header>
+  <div class="main">
+    <h1> <a href="index.html">LauraJohn</a> <em>Our wedding Page</em> </h1>
+  </div>
+  <div class="menu-row">
+    <div class="main">
+      <div class="container_12">
+        <div class="wrapper">
+          <div class="grid_12">
+            <nav>
+              <ul class="menu">
+                <li><a class="active" href="index.html">Головна</a></li>
+                <li><a href="wedding.html">Фотограф</a></li>
+                <li><a href="album.html">Відеооператор</a></li>
+                <li><a href="links.html">Особистий кабінет</a></li>
+                <li><a href="contacts.html">Реєстрація</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="main">
+    <div class="slider-wrapper">
+      <div class="slider">
+        <ul class="items">
+          <li> <img src="images/slider-img1.jpg" alt=""> </li>
+          <li> <img src="images/slider-img2.jpg" alt=""> </li>
+          <li> <img src="images/slider-img3.jpg" alt=""> </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</header>
+<!--==============================content================================-->
 <section id="content">
   <div class="main">
     <div class="container_12">
@@ -5,32 +60,31 @@
         <article class="grid_4">
           <div class="indent-right">
             <h2>Love Story</h2>
-            <p>Laura &amp; John is one of free website templates created by TemplateMonster.com team. This website template is optimized for 1280X1024 screen resolution.</p>
+            
             <ul class="list-1 p2">
-              <li><a href="#">Duis aute irure dolor in reprehenderit</a></li>
-              <li><a href="#">Voluptate velit esse cillum dolore</a></li>
-              <li><a href="#">Cusamus et iusto odio dignissimos</a></li>
-              <li><a href="#">Ducimus qui blanditiis</a></li>
-              <li><a href="#">Praesentium voluptatum deleniti atque</a></li>
+              <li><a href="#">Василь & Роксолана</a></li>
+              <li><a href="#">Іван & Марія</a></li>
+              <li><a href="#">Сергій & Юля</a></li>
+              <li><a href="#">Роман & маша</a></li>
+              <li><a href="#">Славік & Оля</a></li>
             </ul>
-            <p class="img-indent-bot">This Laura &amp; John Template goes with two packages – with PSD source files and without them. PSD source files are avail able for free for the registered members of TemplatesMonster.com.</p>
-            <a class="link-1" href="#">About Our Wedding</a> </div>
+           </div>
         </article>
         <article class="grid_8">
           <div class="indent-left">
-            <h2 class="indent-bot2">About Our Family</h2>
+            <h2 class="indent-bot2">Фотографи</h2>
             <div class="wrapper p3">
               <figure class="img-indent">
-                <p class="p1"><a href="#"><img class="img-border1" src="../../public/images/page1-img1.jpg" alt=""></a><span class="clear"></span></p>
-                <span class="text-1">John Daniels</span> </figure>
+                <p class="p1"><a href="#"><img class="img-border1" src="images/page1-img1.jpg" alt=""></a><span class="clear"></span></p>
+                <span class="text-1">Олександр Ботвиновський</span> </figure>
               <div class="extra-wrap">
                 <blockquote class="q1"> <span class="quote-marker1"></span>
                   <div class="quote-bot">
                     <div class="quote-top">
                       <div class="quote">
                         <div class="padding">
-                          <p class="p2">&quot;...sed ut perspiciatis unde omnis iste natus error volup tatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.</p>
-                          Veritatis et quasi architecto beatae vitae dicta sunt expli cabo. Nemo enim ipsam voluptatem...&quot; </div>
+                          <p class="p2">Студія весільної фотографії Олександри Ботвиновського »- це творчий тандем чудових фотографів - Дмитра та Олександри. Наша творчість дуже тонко поєднує в собі любов до прекрасного і авангардність мислення, що дозволяє найкращим чином передати атмосферу весільного дня. Наш стиль можна описати як легка європейська фотографія, фотографія про любов, адже любов завжди в моді.</p>
+                         </div>
                       </div>
                     </div>
                   </div>
@@ -39,8 +93,8 @@
             </div>
             <div class="wrapper img-indent-bot">
               <figure class="img-indent-r">
-                <p class="p1"><a href="#"><img class="img-border1" src="../../public/images/page1-img2.jpg" alt=""></a><span class="clear"></span></p>
-                <div class="alignright"> <span class="text-1">Laura Daniels</span> </div>
+                <p class="p1"><a href="#"><img class="img-border1" src="images/page1-img2.jpg" alt=""></a><span class="clear"></span></p>
+                <div class="alignright"> <span class="text-1">Наталя Каблюк</span> </div>
               </figure>
               <div class="extra-wrap">
                 <blockquote class="q2"> <span class="quote-marker2"></span>
@@ -48,8 +102,12 @@
                     <div class="quote-top">
                       <div class="quote">
                         <div class="padding">
-                          <p>&quot;Quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos.</p>
-                          Qui ratione voluptatem sequi nesciunt neque poruisquam est, qui dolorem ipsum quia dolor sit amet, consectetur adipisci velit.&quot; </div>
+                          <p>Весільна фотосесія <br>
+♥ Локації. Будь-яке місто України або Європи, теплі краї та регіони безвізового режиму для України :)
+♥ Тривалість.  Можливі два варіанти фотозйомки вашого весілля. Від зборів нареченої до першого танцю або до кінця весілля. 
+♥ Результат.  Біля 1000-1500 фотографій на оформленому диску в повному розмірі, коллажі та зменшені копії для інтернету. Всі фото обробляються :)
+♥ Бонус. Передвесільна фотосесія(лавсторі) як подарунок від мене.</p>
+                         </div>
                       </div>
                     </div>
                   </div>
@@ -62,3 +120,36 @@
     </div>
   </div>
 </section>
+<!--==============================footer=================================-->
+<footer>
+  <div class="main">
+    <div class="container_12">
+      <div class="wrapper">
+        <div class="grid_3 suffix_3">
+          <ul class="list-services">
+            <li><a class="item-1" href="#"></a></li>
+            <li><a class="item-2" href="#"></a></li>
+            <li><a class="item-3" href="#"></a></li>
+          </ul>
+        </div>
+        <div class="grid_5"> <span class="footer-text"> <span>Copyright &copy; Міша Дрогомирецький & Василь Церп'як</span></div>
+      </div>
+    </div>
+  </div>
+</footer>
+<script>
+$(window).load(function () {
+    $('.slider')._TMS({
+        duration: 1000,
+        easing: 'easeOutQuint',
+        preset: 'slideDown',
+        slideshow: 7000,
+        banners: false,
+        pauseOnHover: true,
+        pagination: true,
+        pagNums: false
+    });
+});
+</script>
+</body>
+</html>
